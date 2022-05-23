1ffc11079fcc45d6c01cb3845de23cbd9198161d
AhmedMaad <atef.ahmed1341@gmail.com>
2022-05-23 19:37:44 +0200

    password toggle for edit text

--- app/src/main/java/com/maad/worldtickets/SignUpActivity.kt
+++ app/src/main/java/com/maad/worldtickets/SignUpActivity.kt
@@ -1,57 +1,69 @@
 package com.maad.worldtickets
 
-import android.R
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.method.PasswordTransformationMethod
+import android.text.InputType
 import android.view.MotionEvent
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.maad.worldtickets.databinding.ActivitySignUpBinding
 
-
 class SignUpActivity : AppCompatActivity() {
+
+    var isPasswordVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.passwordEt.setDrawableRightTouch {
 
-        /*etPassword.setOnTouchListener(object : OnTouchListener() {
-            fun onTouch(view: View?, event: MotionEvent): Boolean {
-                //final int DRAWABLE_LEFT = 0;
-                //final int DRAWABLE_TOP = 1;
-                val DRAWABLE_RIGHT = 2
-                //final int DRAWABLE_BOTTOM = 3;
-                if (event.action == MotionEvent.ACTION_UP) {
-                    if (event.rawX >= etPassword.getRight() - etPassword.getCompoundDrawables()
-                            .get(DRAWABLE_RIGHT).getBounds().width()
-                    ) {
-                        // your action here
-                        if (counter % 2 === 0) {
-                            etPassword.setCompoundDrawablesWithIntrinsicBounds(
-                                0,
-                                0,
-                                R.drawable.ic_visibility_black_24dp,
-                                0
-                            )
-                            etPassword.setTransformationMethod(null) //shows the password
-                        } else {
-                            etPassword.setCompoundDrawablesWithIntrinsicBounds(
-                                0,
-                                0,
-                                R.drawable.ic_visibility_off_black_24dp,
-                                0
-                            )
-                            etPassword.setTransformationMethod(PasswordTransformationMethod()) //hides the password
-                        }
-                        ++counter
-                        return true
-                    }
-                }
-                return false
+            if (!isPasswordVisible) {
+                binding.passwordEt.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                binding.passwordEt.setCompoundDrawablesWithIntrinsicBounds(
+                    0,
+                    0,
+                    R.drawable.ic_visibility_on,
+                    0
+                )
+                isPasswordVisible = true
+                binding.passwordEt.setSelection(binding.passwordEt.length())
+            } else {
+                binding.passwordEt.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                binding.passwordEt.setCompoundDrawablesWithIntrinsicBounds(
+                    0,
+                    0,
+                    R.drawable.ic_visibility_off,
+                    0
+                )
+                isPasswordVisible = false
+                binding.passwordEt.setSelection(binding.passwordEt.length())
             }
-        })*/
 
+        }
+
+
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    fun EditText.setDrawableRightTouch(setClickListener: () -> Unit) {
+        this.setOnTouchListener(View.OnTouchListener { _, event ->
+            val DRAWABLE_LEFT = 0
+            val DRAWABLE_TOP = 1
+            val DRAWABLE_RIGHT = 2
+            val DRAWABLE_BOTTOM = 3
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= this.right - this.compoundDrawables[DRAWABLE_RIGHT].bounds.width()
+                ) {
+                    setClickListener()
+                    return@OnTouchListener true
+                }
+            }
+            false
+        })
     }
 
 }
