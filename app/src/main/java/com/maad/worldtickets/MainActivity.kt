c432ab1381bb9e852dbcacdcfebbadad3efed9a3
AhmedMaad <atef.ahmed1341@gmail.com>
2022-05-23 14:23:17 +0200

    initial commit

---
+++ app/src/main/java/com/maad/worldtickets/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.maad.worldtickets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maad.worldtickets.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+
+}
