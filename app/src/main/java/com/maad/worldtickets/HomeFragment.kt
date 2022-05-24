e70ebb8a7cc868ab91e2445908ce455c12c4386f
AhmedMaad <atef.ahmed1341@gmail.com>
2022-05-24 14:44:56 +0200

    check commit

--- app/src/main/java/com/maad/worldtickets/HomeFragment.kt
+++ app/src/main/java/com/maad/worldtickets/HomeFragment.kt
@@ -1,21 +1,44 @@
 package com.maad.worldtickets
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.maad.worldtickets.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
+        db = Firebase.firestore
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.profileIv.setOnClickListener {
+            val i = Intent(context, ProfileActivity::class.java)
+            startActivity(i)
+        }
+
+        val prefs = requireContext().getSharedPreferences("settings", Context.MODE_PRIVATE)
+        val id = prefs.getString("id", null)!!
+
+        db.collection("users").document(id).get().addOnSuccessListener {
+            binding.progress.visibility = View.GONE
+            val user = it.toObject(User::class.java)!!
+            if (user.pp.isEmpty()){}
+
+        }
+
         return binding.root
     }
 
