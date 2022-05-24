e0ef6cc390de1658177b9162e9a29958ec17a956
AhmedMaad <atef.ahmed1341@gmail.com>
2022-05-24 15:57:03 +0200

    profile design

--- app/src/main/java/com/maad/worldtickets/ProfileActivity.kt
+++ app/src/main/java/com/maad/worldtickets/ProfileActivity.kt
@@ -2,10 +2,17 @@ package com.maad.worldtickets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.maad.worldtickets.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        val binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backIv.setOnClickListener { finish() }
+
+        //update users info, including (email from update email in firebase auth if needed)
+
     }
 }
