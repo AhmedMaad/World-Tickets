3286c721a3621ce557f8a04cb115273ff3f1cf62
AhmedMaad <atef.ahmed1341@gmail.com>
2022-05-27 23:17:26 +0200

    calendar design

--- app/src/main/java/com/maad/worldtickets/CalendarFragment.kt
+++ app/src/main/java/com/maad/worldtickets/CalendarFragment.kt
@@ -1,11 +1,13 @@
 package com.maad.worldtickets
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.maad.worldtickets.databinding.FragmentCalendarBinding
+import java.util.*
 
 class CalendarFragment : Fragment() {
 
@@ -15,6 +17,21 @@ class CalendarFragment : Fragment() {
 
         val binding = FragmentCalendarBinding.inflate(inflater, container, false)
 
+        //binding.calendar.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->  }
+
+        val c = Calendar.getInstance()
+        binding.calendar.init(
+            c.get(Calendar.YEAR),
+            c.get(Calendar.MONTH),
+            c.get(Calendar.DAY_OF_MONTH)
+        ) { view, year, monthOfYear, dayOfMonth ->
+            if (dayOfMonth % 10 == 0){
+
+            }
+            else if (dayOfMonth % 7 == 0){
+
+            }
+        }
 
         return binding.root
     }
